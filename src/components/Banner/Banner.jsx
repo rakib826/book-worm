59dcf201f5611f14ec8082831a7fb52e919aad12
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-02-10 00:58:52 +0600

    added button to book details

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
           <img src={bannerImg} className="max-w-sm rounded-lg shadow-2xl" />
           <div className="">
             <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6 lg:mr-20">
+            <p className="py-6 pr-20">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
