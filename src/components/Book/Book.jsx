8d1a76eae07321f2b4ec9175928d0ce456214a97
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-02-09 19:54:37 +0600

    added books ratings

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Book = ({ book }) => {
-  const { image, bookName, author } = book;
+  const { image, bookName, author, tags, category, rating } = book;
 
   return (
     <div>
@@ -14,13 +14,21 @@ const Book = ({ book }) => {
           />
         </figure>
         <div className="card-body flex flex-col flex-grow justify-start">
-          <h2 className="card-title">
+          <div>
+            {tags.map((tag) => (
+              <button className="py-1 px-2 bg-lime-100 mr-2 rounded-md italic text-sm">{tag}</button>
+            ))}
+          </div>
+          <h2 className="card-title font-bold">
             {bookName}
-            <div className="badge badge-secondary">NEW</div>
+            {/* <div className="badge badge-secondary">NEW</div> */}
           </h2>
-          <p className="flex-grow">
-            {author}
-          </p>
+          <p className="flex-grow italic">By {author}</p>
+          <div className="border-b-2 my-1"></div>
+          <div className="flex flex-row justify-end items-center">
+            <p> {category} </p>
+            <p className="text-right"> {rating} </p>
+          </div>
           <div className="card-actions justify-end mt-auto">
             <div className="badge badge-outline">Fashion</div>
             <div className="badge badge-outline">Products</div>
