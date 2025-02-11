b19bc4c4e4122e758b9dde98fe34628c656e4dd9
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-02-11 20:25:49 +0600

    local storage function created

---
+++ src/utility/addToDb.js
@@ -0,0 +1,19 @@
+const getStoredReadList = () => {
+  const storedListStr = localStorage.getItem('read-list');
+  if(storedListStr){
+    const storedList = JSON.parse(storedListStr);
+    return storedList
+  }else{
+    return [];
+  }
+}
+
+const addToStoredReadList = (id) => {
+  const storedList = getStoredReadList();
+  if(storedList.includes(id)){
+    console.log(id,"Alreday exist in the read list")
+  }else{
+    storedList.push(id)
+    const new
+  }
+}
